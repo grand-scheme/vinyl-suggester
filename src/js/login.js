3eb861f2f29ada01bc09701f52a8f2085bd85710
Eric Stratton <strattonericj@gmail.com>
2020-12-02 11:44:57 -0800

    attempt to implement login for authentication
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

---
+++ src/js/login.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//   $('#login').click(function() {
+    
+//     const client_id = '56346d067885461f91b9a4b099c49290';
+//     const redirect_uri = 'http://localhost:8080/callback';
+    
+//     const scope = 'user-read-email user-read-private';
+//     const url = 'GET https://accounts.spotify.com/authorize';
+//       url += '?client_id=' + encodeURIComponent(client_id);
+//       url += '&response_type=code';
+//       url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+//       url += '&scope=' + encodeURIComponent(scope);
+//       window.location = url;
+
+//   });
+// });
--- src/main.js
+++ src/main.js
@@ -22,6 +22,22 @@ function spotifyPlayback(albumID) {
 
 // User Interface Logic //
 
+$(document).ready(function() {
+  $('#login').click(function() {
+    
+    const client_id = '56346d067885461f91b9a4b099c49290';
+    const redirect_uri = 'http://localhost:8080/callback';
+    
+    const scope = 'user-read-email user-read-private';
+    let url = 'https://accounts.spotify.com/authorize';
+    url += '?client_id=' + encodeURIComponent(client_id);
+    url += '&response_type=code';
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&scope=' + encodeURIComponent(scope);
+    window.location = url;
+  });
+});
+
 $('#readMore').click (function() {
   $('#intro').fadeOut();
   $('#output').fadeIn();
