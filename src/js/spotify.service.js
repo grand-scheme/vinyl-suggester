39cd87891a2fde903dfcb4f85e783773d1a6764a
Zahnen Garner <zahnen@gmail.com>
2020-12-03 08:54:10 -0800

    prep for merge with master

--- src/js/spotify.service.js
+++ src/js/spotify.service.js
@@ -1,7 +1,7 @@
 export default class Spotify {
   static searchSpotify(keyword) {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer BQCj3IqYLUDzNZvHpdoOcdiJtF9aWKUPuUkOTg6Yk4jgI6BFbt93spoDryhujTHTzdp-k_nm0wtntZdPeiBtIhmZOC80FodlfhGszn9FKPtBOjrFBXCa9qgYlY0PQwZJuo5UoaQt1E5INNTu3csQbaSVgVCGR7Va");
+    myHeaders.append("Authorization", "Bearer BQBnp23h0K44CcPerK_hKXgyJhdjxp7IolB92RCtZpyLjM0FSE-yv1YXUI_rgket4TbR_fOS_joHQjAYpbw3O-JLC8Gcs6tyvJ4oVigcflAtwJI-FlyQH4T98vXqsEdCEVboZlsRkK0cer9gG5ve-IiQd169_KBE");
     myHeaders.append("Cookie", "_ga=GA1.2.456045822.1606765238; _gid=GA1.2.637614845.1606765238; sp_dc=AQB5RMpfdYpkmkJlRHPeYxSSfyTRPpkKySEvxhbkmixFgyFT0ZOlFeh5KY4xSVAXFpCp276qTTx4nbxD11fqn-6DnKhR9PuJ6yLMzkXUPg; sp_key=66b31764-29a3-4c2f-92c8-38ed42c7b352; _gat=1");
 
     const requestOptions = {
